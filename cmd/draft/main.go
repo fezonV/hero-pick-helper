9cb8192d4d68c8d9b4f683e475b6fd17859a1733
fezonV <mr.razor2706@gmail.com>
2026-09-05 00:42:20 +0300

    Добавил возможность вывода всех героев на экран

--- cmd/draft/main.go
+++ cmd/draft/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/fezonV/hero-pick-helper/service"
+)
+
+func main() {
+	heroes, err := service.GetHeroes()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	for i, hero := range heroes {
+		fmt.Println(i, hero.Localized_name)
+	}
 }
--- service/api.go
+++ service/api.go
@@ -1,11 +1,31 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/fezonV/hero-pick-helper/internal/hero"
 )
 
-func ParseOpenDota() ([]hero.HeroModel, error) {
-	resp, err := http.Get("http://example.com")
+func GetHeroes() ([]hero.HeroModel, error) {
+	resp, err := http.Get("https://api.opendota.com/api/heroes")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code %d", resp.StatusCode)
+	}
+
+	var heroes []hero.HeroModel
+
+	err = json.NewDecoder(resp.Body).Decode(&heroes)
+	if err != nil {
+		return nil, err
+	}
+	return heroes, nil
 }
