bfcb2e4dbbee565ef2a40b140435ecbe8ecaa935
fezonV <mr.razor2706@gmail.com>
2026-09-05 13:52:50 +0300

    Добавил возможность смотреть статистику героев

--- cmd/draft/main.go
+++ cmd/draft/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/fezonV/hero-pick-helper/service"
 )
 
 func main() {
-	heroes, err := service.GetHeroes()
+	ctx := context.Background()
+	stats, err := service.GetHeroStats(ctx)
+
+	var ID int64
+	fmt.Print("Введите номер героя: ")
+	fmt.Scan(&ID)
+
+	stat, ok := stats[ID]
+	if !ok {
+		fmt.Println("Герой с таким ID не найден")
+		return
+	}
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	for i, hero := range heroes {
-		fmt.Println(i, hero.Localized_name)
+	wr, err := stat.Winrate(4)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
 	}
+	fmt.Println("Имя:", stat.Localized_name, "Винрейт на титанах:", wr*100, "%")
 }
